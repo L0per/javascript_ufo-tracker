2107f4bab4a29def6a4d4ddc5573cf3b9c4939a2
Loper <jloper@ucsc.edu>
2020-01-13 13:35:34 -0800

    level 1 completed

---
+++ static/js/app.js
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////////////////////////////
+// Data and d3 selections
+
+// from data.js
+let tableData = data;
+
+// select the table body
+let table = d3.select("#ufo-table").select("tbody");
+
+// text header if no text is found
+let notFound = d3.select("#table-area").append("h2")
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Initial table creation
+
+// iterate through tableData and add new table row for each sighting
+tableData.forEach(sighting => {
+
+    // add new row for each sighting
+    let row = table.append("tr");
+    // create array contianing values for each sighting
+    let sightingEntry = Object.values(sighting);
+
+    // iterate though each sighting value
+    sightingEntry.forEach(sightingValue => {
+        // add new td text for each value in the array
+        row.append("td").text(sightingValue); 
+    })
+})
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Filtering table
+
+// select the filter button
+let button = d3.select("#filter-btn");
+
+// trigger when filter button is clicked
+button.on("click", function() {
+
+    // select date input field
+    let inputDate = d3.select("#datetime");
+    // select date input field values
+    let inputDateValue = inputDate.property("value");
+
+    // filter data based on input date
+    let filteredDates = tableData.filter(sighting => sighting.datetime === inputDateValue);
+
+    // clear table
+    table.html("")
+    // clear not found text
+    notFound.html("")
+
+    // //Refresh table
+    filteredDates.forEach(sighting => {
+
+        // add new row for each sighting
+        let row = table.append("tr");
+        // create array contianing values for each sighting
+        let sightingEntry = Object.values(sighting);
+    
+        // iterate though each sighting value
+        sightingEntry.forEach(sightingValue => {
+            // add new td text for each value in the array
+            row.append("td").text(sightingValue); 
+        })
+    })
+
+    // text if no results found
+    if (filteredDates.length === 0) {
+        notFound.text("No sightings found!")
+    }
+
+})
+
